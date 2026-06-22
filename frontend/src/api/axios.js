2bdd0739083a7a7246a55ea63d0476f073ba4e1b
ashutosh-mishra7 <mishraashutosh9199@gmail.com>
2026-06-22 14:12:47 +0530

    s

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://devtrack-nd76.onrender.com/api',
+  timeout: 10000,
 });
 
 api.interceptors.request.use((config) => {
